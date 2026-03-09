00b0d84cf3b763fcd6b9d7e10676aef3b64c6dba
darth.revan.030 <smartsamarth03@gmail.com>
2026-03-09 22:44:47 +0800

    Add UserService instance and setup for user creation endpoint

--- cmd/go-server-trial/main.go
+++ cmd/go-server-trial/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/darthrevan030/go-server-trial/internal/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
 )
 
 func main() {
+
+	// userservice instance
+	UserService := service.UserService{}
+
+	
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
@@ -19,6 +24,8 @@ func main() {
 	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("Welcome"))	
 	})
+
+	router.Post("api/v1/users", )
 	
 	http.ListenAndServe(":3000", router)
 }
