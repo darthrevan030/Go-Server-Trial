eac7a03acf1c77439337e7a9ae641ea60f91cc07
darth.revan.030 <smartsamarth03@gmail.com>
2026-03-09 22:17:24 +0800

    Add MongoClient struct and CRUD methods for user management

---
+++ internal/mongodb/user.go
@@ -0,0 +1,34 @@
+package mongodb
+
+import (
+	"github.com/darthrevan030/go-server-trial/internal/model"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MongoClient struct {
+	Client mongo.Collection
+}
+
+func (c MongoClient) CreateUser(user model.User) (string, error) {
+	return "", nil
+}
+
+func (c MongoClient) GetUserByID(user string) (model.User, error) {
+	return model.User{}, nil
+}
+
+func (c MongoClient) GetAllUsers() ([]model.User, error) {
+	return []model.User{}, nil
+}
+
+func (c MongoClient) UpdateUserAgeByID(id string, age int) (int, error) {
+	return 0, nil
+}
+
+func (c MongoClient) DeleteUserByID(id string) (int, error) {
+	return 0, nil
+}
+
+func (c MongoClient) DeleteAllUsers() (int, error) {
+	return 0, nil
+}
