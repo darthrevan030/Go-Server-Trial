aed2a1ce8464eed367475e32bb408e58346abb55
darth.revan.030 <smartsamarth03@gmail.com>
2026-03-23 23:00:01 +0800

    Refactor MongoDB connection handling and improve environment variable loading

--- cmd/go-server-trial/main.go
+++ cmd/go-server-trial/main.go
@@ -2,44 +2,62 @@ package main
 
 import (
 	"context"
-	"time"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/darthrevan030/go-server-trial/internal/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
+func init() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error While Reading Environment Variables")
+	}
+
+	slog.Info("Environment Variables Loaded Successfully")
+}
+
+
 func mongoConnectionOpen() *mongo.Client {
+    serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+    opts := options.Client().ApplyURI(os.Getenv("MONGODB_URI")).SetServerAPIOptions(serverAPI)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+    client, err := mongo.Connect(opts)
+    if err != nil {
+        log.Fatal("Failed to connect to MongoDB: ", err)
+    }
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/go-server-trial"))
+    // ping to verify
+    var result bson.M
+    if err := client.Database(os.Getenv("MONGODB_URI")).RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+        log.Fatal("MongoDB ping failed: ", err)
+    }
 
-	if err != nil {
-		log.Fatal("Failed to connect to MongoDB: ", err)
-	}
-
-	// verify connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal("MongoDB ping failed: ", err)
-	}
-
-	log.Println("Connected to MongoDB")
-	return client
-} 
+    log.Println("Connected to MongoDB!")
+    return client
+}
 
 
 func main() {
 
+	client := mongoConnectionOpen()
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		client.Disconnect(ctx)
+	} ()
+
 	// userservice instance
 	UserService := service.UserService{}
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
@@ -55,11 +73,12 @@ func main() {
 		r.Post("/users", UserService.CreateUser)
 		r.Get("/users", UserService.GetAllUsers)
 		r.Get("/users/{id}", UserService.GetUserByID)
-		r.Put("/users{id}", UserService.UpdateUserAgeByID)
+		r.Put("/users/{id}", UserService.UpdateUserAgeByID)
 		r.Delete("/users/{id}", UserService.DeleteUserByID)
 		r.Delete("/users", UserService.DeleteAllUsers)
 	
 	})
-
+	log.Println("Server running on :3000")  
+    log.Fatal(http.ListenAndServe(":3000", r))
 	http.ListenAndServe(":3000", r)
 }
