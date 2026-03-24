c0da1e0687214a2013ed0294de6e61c442828fba
darth.revan.030 <smartsamarth03@gmail.com>
2026-03-24 21:20:22 +0800

    Implement UpdateUserAgeByID function with request body decoding and response handling

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -90,7 +90,29 @@ func (h Handler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h Handler) UpdateUserAgeByID(w http.ResponseWriter, r *http.Request) {
+	
+	// decode the request
+	id := chi.URLParam(r, "id") 
 
+	var req UserRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+	http.Error(w, "Invalid request body", http.StatusBadRequest) // 400
+	return
+}
+
+	// call the repo
+	result, err := h.repo.UpdateUserAgeByID(id, req.Age)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError) // 500
+		return
+	}
+
+	// send response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+	json.NewEncoder(w).Encode(UserResponse{
+		Data: result,
+	})
 }
 
 func (h Handler) DeleteUserByID(w http.ResponseWriter, r *http.Request) {
